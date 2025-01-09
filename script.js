ccebadeacf3afab5d5d0999c999d3e68a34cde83
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2025-01-09 01:13:26 -0300

    refactor(script): Mudança para POO, criação da classe ModalController

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const modal = document.querySelector('.modal-container');
-const customerForm = document.querySelector('modal');
+class ModalController {
+    constructor() {
+        this.modal = document.querySelector('.modal-container');
+        this.btnOpen = document.querySelector('#new');
+        this.btnOpen.addEventListener('click', () => this.openModal());
+        this.modal.addEventListener('click', () => this.closeModal());
+    }
 
-function openCloseModal() {
-    modal.classList.add('active')
-    closeModal()
-}
+    openModal() {
+        this.modal.classList.add('active');
+    }
 
-function closeModal() {
-    modal.onclick = e => {
-        if (e.target.classList.contains('modal-container')) {
-          modal.classList.remove('active')
+    closeModal() {
+        this.modal.onclick = e => {
+            if (e.target.classList.contains('modal-container')) {
+                this.modal.classList.remove('active')
+            }
         }
     }
 }
+const modal = new ModalController();
+
+
+
+
+
